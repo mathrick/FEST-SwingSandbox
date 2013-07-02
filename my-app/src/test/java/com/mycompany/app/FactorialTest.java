8aa42caaafe782b7b93c03a8e35e96a46eaa98bc
Maciej Katafiasz <mathrick@gmail.com>
2013-07-02 15:32:36 +0200

    Convert values test to use @Parameters

--- my-app/src/test/java/com/mycompany/app/FactorialTest.java
+++ my-app/src/test/java/com/mycompany/app/FactorialTest.java
@@ -1,30 +1,13 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertEquals;
-
-import java.math.BigInteger;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class FactorialTest 
-{
+public class FactorialTest {
+
 	@Test(expected=NegativeFactorialException.class)
     public void noNegativeFactorial()
     {
         Factorial.factorial(-1);
     }
-	
-	@Test
-	public void computeFactorials()
-	{
-		assertEquals(BigInteger.ONE, Factorial.factorial(0));
-		assertEquals(BigInteger.ONE, Factorial.factorial(1));
-		assertEquals(BigInteger.valueOf(2), Factorial.factorial(2));
-		assertEquals(BigInteger.valueOf(6), Factorial.factorial(3));
-		assertEquals(BigInteger.valueOf(120), Factorial.factorial(5));
-		assertEquals(BigInteger.valueOf(720), Factorial.factorial(6));
-	}
+
 }
---
+++ my-app/src/test/java/com/mycompany/app/FactorialValuesTest.java
@@ -0,0 +1,44 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(Parameterized.class)
+public class FactorialValuesTest 
+{
+	private int arg, result;
+	
+    @Parameters(name="{index}: {0}! = {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { 
+        		{ 0, 1 }, { 1, 1 }, 
+        		{ 2, 2 }, { 3, 6 }, 
+                { 5, 120 }, { 6, 720 }, 
+                { 12, 479001600 } 
+                });
+    }
+
+    public FactorialValuesTest(int n, int expected)
+    {
+    	arg = n;
+    	result = expected;
+    }
+    
+	@Test
+	public void computeFactorials()
+	{
+		assertEquals(BigInteger.valueOf(result), Factorial.factorial(arg));
+	}
+
+}
